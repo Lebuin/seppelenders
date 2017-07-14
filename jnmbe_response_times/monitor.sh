5d0b41a1b53548ba9cadc76eec3e487cbe10a438
Lebuin <seppe.lenders@gmail.com>
2017-07-14 17:40:53 +0200

    fix newlines

--- jnmbe_response_times/monitor.sh
+++ jnmbe_response_times/monitor.sh
@@ -7,4 +7,4 @@ if [ ! -f $f_output ]; then
     echo "date,time_namelookup,time_connect,time_appconnect,time_pretransfer,time_redirect,time_starttransfer,time_total" > $f_output
 fi
 
-curl -w $(date -u +%Y-%m-%dT%H:%M:%SZ),%{time_namelookup},%{time_connect},%{time_appconnect},%{time_pretransfer},%{time_redirect},%{time_starttransfer},%{time_total} -o /dev/null -s $url >> $f_output
+curl -w $(date -u +%Y-%m-%dT%H:%M:%SZ),%{time_namelookup},%{time_connect},%{time_appconnect},%{time_pretransfer},%{time_redirect},%{time_starttransfer},%{time_total}\\n -o /dev/null -s $url >> $f_output
