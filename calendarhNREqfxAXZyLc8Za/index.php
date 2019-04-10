1497a4c5afd09b8846434da727aab6501211c3b6
Lebuin <seppe.lenders@gmail.com>
2019-04-10 19:58:21 +0200

    add Vectera calendar

--- calendarhNREqfxAXZyLc8Za/index.php
+++ calendarhNREqfxAXZyLc8Za/index.php
@@ -55,6 +55,7 @@ switch($name) {
         $urls = array(
             '' => 'https://calendar.google.com/calendar/ical/seppe.lenders%40gmail.com/private-0fad6d2cbac026c1311c2a5f25967e76/basic.ics',
             '(JNM) ' => 'https://calendar.google.com/calendar/ical/allip39gmffdvs6i0hskfq7i7k%40group.calendar.google.com/private-b21c7801622f68df33ba4aaf018b4a64/basic.ics',
+            '(werkdag) ' => 'https://calendar.google.com/calendar/ical/vectera.com_865883fn1rf955at75kjpetg4o%40group.calendar.google.com/private-7a8617424c831c6906001dfaca007a3b/basic.ics',
             '(Vectera) ' => 'https://calendar.google.com/calendar/ical/vectera.com_865883fn1rf955at75kjpetg4o%40group.calendar.google.com/private-7a8617424c831c6906001dfaca007a3b/basic.ics',
             '(Biotope) ' => 'https://calendar.google.com/calendar/ical/1ubt4d8hk5h7b6cgbpq6jjh3js%40group.calendar.google.com/private-e49eaf2dd869c6692964ac1a8aed4a30/basic.ics',
         );
